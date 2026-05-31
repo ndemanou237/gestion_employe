cf6dfedd2dc0e1b2bff78d1e6b90fff1d668ec7d
ndemanou237 <ndemanoudonald9@gmail.com>
2026-05-31 21:23:40 +0100

    creation de la views ajouter_etudiant

--- employe/views.py
+++ employe/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Employe
+from .forms import EmployeForm
 
 def liste_employes(request):
     employes = Employe.objects.all()
@@ -7,3 +8,14 @@ def liste_employes(request):
         'employes':employes
         }
     return render(request, "employe/list.html", context)
+
+def ajouter_employer(request):
+    form = EmployeForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('liste_employes')
+    context = {
+        'form':form
+        }
+    return render(request, "employe/formulaire.html", context)
+
